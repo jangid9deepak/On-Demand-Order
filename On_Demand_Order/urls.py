964a1e02530ce608b9afc16017e8cd528c156165
jangid9deepak <jangid9deepak@gmail.com>
2026-08-27 13:55:59 +0530

    Serve uploaded media files in production

--- On_Demand_Order/urls.py
+++ On_Demand_Order/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -64,7 +66,10 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    )
+]
